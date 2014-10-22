c12106c51d5cbcc98743367d67c3abc6eb4dc09e
Moritz Schmidt <fusselwurm@gmail.com>
2014-10-22 23:11:58 +0200

    wip getting missions

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 #!/bin/env node
 
-var restify = require('restify');
+var restify = require('restify'),
+    url = require('url'),
+    crypto = require('crypto'),
+    missionFetcher = require('./lib/mission-fetcher.js'),
+    baseUrl = 'http://localhost:8080',
+    missions = {},
+    errorUrls = {};
 
 
 function respondHelllo(req, res, next) {
@@ -8,8 +14,43 @@ function respondHelllo(req, res, next) {
     next();
 }
 
+function register(digest, url) {
+    missions[digest] = url;
+    missionFetcher.fetchHttp(url, function (err, data) {
+        if (err) {
+            delete missions[digest];
+            if (err.code === 'ENOTFOUND') {
+                errorUrls[digest] = 404;
+            }
+            errorUrls[digest] = 500;
+        }
+        console.log('successfully fetched ' + url);
+    });
+}
+
+function validUrl(string) {
+    var bits = url.parse(string);
+
+    return bits.protocol === 'http:' && bits.host && bits.path;
+}
+
 function registerUrl(req, res, next) {
-    console.log(req);
+    var
+        missionUrl = req.params && req.params.url,
+        hash = crypto.createHash('sha1'),
+        digest;
+
+    hash.update(missionUrl);
+    digest = hash.digest('hex');
+    if (!missionUrl) {
+        res.send(400);
+    } else if (!validUrl(missionUrl)) {
+        res.send(400);
+    } else {
+        register(digest, missionUrl);
+        res.send(201, {location: baseUrl + '/mission/' + digest})
+    }
+    next();
 }
 
 function getMissions(req, res, next) {
@@ -18,10 +59,11 @@ function getMissions(req, res, next) {
 }
 
 var server = restify.createServer();
+server.use(restify.bodyParser());
 server.get('/hello/:name', respondHelllo);
 server.head('/hello/:name', respondHelllo);
 
-server.post('/register/', registerUrl);
+server.post('/register', registerUrl);
 server.get('/missions/', getMissions);
 
 server.listen(8080, function() {
---
+++ test/hello_spec.js
@@ -0,0 +1,23 @@
+var
+    frisby = require('frisby'),
+    endpoint = 'http://localhost:8080';
+
+frisby.create('get "hello" moo')
+    .get(endpoint + '/hello/moo')
+    .expectStatus(200)
+    .expectHeaderContains('content-type', 'application/json')
+    .expectBodyContains("hello moo")
+    .toss();
+
+
+frisby.
+    create('register invalid URL').
+    post(endpoint + '/register', {url: 'meine-datei.pbo'}).
+    expectStatus(400).
+    toss();
+
+frisby.
+    create('register URL that does not exist').
+    post(endpoint + '/register', {url: 'http://moo.test/doesnotexist'}).
+    expectStatus(201).
+    toss();
